f4f60382e89f90fc7362676dd5e46cba9268765d
ELMER IVAN MAMANI TICONA <elmerivan7@gmail.com>
2022-09-06 22:00:25 -0400

    Servicio PUT para productos /api/v1/products/:id

--- controllers/productController.js
+++ controllers/productController.js
@@ -46,15 +46,47 @@ exports.getProductById = (req, res) => {
     });
   }
 };
+   // Leer los datos y modificarlos desde postman segun el id (PUT)
+exports.modificaProductoId = (req, res) => {
+  const products = JSON.parse(
+    fs.readFileSync(`${__dirname}/../data/products.json`)
+  );
 
+  const foundProduct = products.find((p) => p.id == req.params.id);
+    if (foundProduct) {
+          for(var i = 0; i < products.length;i++){
+              if(foundProduct.id == products[i].id){
+                  console.log('id el mismo');
+                  for(var key in req.body){
+                      if(products[i][key]){
+                        products[i][key] = req.body[key];
+                      }
+                  }
+              }
+          }
+      // Guardando Datos desde Postman segun el id capturado y modificado
+      let json_product = JSON.stringify(products);
+      fs.writeFileSync(`${__dirname}/../data/products.json`, json_product);
+      res.status(201).json({
+        status: "actualizado correctamente producto "+req.params.id,
+      });
+    } else {
+      res.status(404).json({
+        status: "not found",
+      });
+    }
+  };
+    // Borrando Datos desde Postman segun el id capturado (DELETE)
 exports.borrarProductoId = (req, res) => { 
   let productos = JSON.parse( fs.readFileSync(`${__dirname}/../data/products.json`) ); 
 const foundProduct = productos.find((p) => p.id == req.params.id); 
   if (foundProduct) { 
     productos = productos.filter((prod) => prod.id != req.params.id); 
     fs.writeFileSync(`${__dirname}/../data/products.json`, JSON.stringify(productos)); 
-    res.status(204).json({ status: "success "}); 
-  } 
-  else 
-  { res.status(404).json({ status: "not found", }); 
+    res.status(201).json({ 
+      status: "Eliminado producto "+req.params.id,
+    }); 
+  } else { 
+    res.status(404).json({ 
+      status: "not found", });
 }};
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -7,7 +7,7 @@ productRouter
   .get(productController.getAllProducts)
   .post(productController.addProduct);
 productRouter.route("/:id").get(productController.getProductById);
-
+productRouter.route("/:id").put(productController.modificaProductoId)
 productRouter.route("/:id").delete(productController.borrarProductoId)
 
 module.exports = productRouter;
